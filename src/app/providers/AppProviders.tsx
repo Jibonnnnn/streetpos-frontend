10aef6af0530cf8799236f271f995c228175edaa
Jibonnnnn. <jeevonricafort@gmail.com>
2026-07-30 23:23:26 +0800

    removed unused imports

--- src/app/providers/AppProviders.tsx
+++ src/app/providers/AppProviders.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'sonner';
 import { CartProvider } from '@/contexts/CartContext';
 import type { ReactNode } from 'react';
